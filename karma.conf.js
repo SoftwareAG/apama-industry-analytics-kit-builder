4f6b709765fe33887484e00735ff46f89bfbba7f
RPEA <richard.peach@softwareag.com>
2017-03-28 10:57:06 +0100

    Adding Jenkins support

--- karma.conf.js
+++ karma.conf.js
@@ -15,7 +15,8 @@ module.exports = function (config) {
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
-      require('@angular/cli/plugins/karma')
+      require('@angular/cli/plugins/karma'),
+      require('karma-junit-reporter')
     ],
     client:{
       clearContext: false // leave Jasmine Spec Runner output visible in browser
@@ -37,13 +38,17 @@ module.exports = function (config) {
       environment: 'dev'
     },
     reporters: config.angularCli && config.angularCli.codeCoverage
-              ? ['progress', 'coverage-istanbul']
-              : ['progress', 'kjhtml'],
+              ? ['progress', 'coverage-istanbul', 'dots', 'junit']
+              : ['progress', 'kjhtml', 'dots', 'junit'],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: false,
     browsers: ['Chrome'],
-    singleRun: true
+    singleRun: true,
+    junitReporter: {
+      outputDir: "test-results",
+      outputFile: "test-results.xml"
+    }
   });
 };
