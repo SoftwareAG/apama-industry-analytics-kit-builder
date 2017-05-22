9e26d346f7c3b561a29784a61a6dad35bb05e578
RPEA <richard.peach@softwareag.com>
2017-05-22 14:11:33 +0100

    Tidying analytic accordion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,7 +9,7 @@ import {TransformerSelectorComponent} from "./widgets/transformer-selector/trans
 import {LadderDiagramComponent} from "./widgets/ladder-diagram/ladder-diagram.component";
 import {AbstractDataService} from "./services/AbstractDataService";
 import {DataService} from "app/services/DataService";
-import {NgbAccordion, NgbModule} from "@ng-bootstrap/ng-bootstrap";
+import {NgbModule} from "@ng-bootstrap/ng-bootstrap";
 import {TransformerPropertySelectorComponent} from "./widgets/transformer-property-selector/transformer-property-selector.component";
 import {NavBarComponent} from "./widgets/nav-bar/nav-bar.component";
 import {AbstractDragService} from "app/services/AbstractDragService";
@@ -21,9 +21,9 @@ import {TransformerSerializer} from "./classes/Transformer";
 import {PropertySerializer} from "./classes/Property";
 import {FileService} from "./services/FileService";
 import {AbstractMetadataService, MetadataService} from "./services/MetadataService";
-import {SaveConfigurationComponent } from './widgets/save-configuration/save-configuration.component';
+import {SaveConfigurationComponent} from "./widgets/save-configuration/save-configuration.component";
 import {FocusDirective} from "./directives/focus.directive";
-import { InformationComponent } from './widgets/information/information.component';
+import {InformationComponent} from "./widgets/information/information.component";
 import {TransformerDefinitionComponent} from "./widgets/transformer-definition/transformer-definition.component";
 import {ResizableModule} from "angular-resizable-element";
 
