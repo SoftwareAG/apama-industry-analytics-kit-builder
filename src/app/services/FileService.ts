60ebe50a723561c840960d9b5d712aec6cd39a19
RPEA <richard.peach@softwareag.com>
2017-07-11 10:27:32 +0100

    fixing file save and file open in ff and ie

--- src/app/services/FileService.ts
+++ src/app/services/FileService.ts
@@ -7,6 +7,7 @@ import {Metadata, MetadataJsonInterface} from "../classes/Metadata";
 import * as Promise from "bluebird";
 import {Utils} from "../Utils";
 import {TransformerDefBuilder, TransformerDefJsonInterface} from "../classes/TransformerDef";
+import * as FileSaver from 'file-saver';
 
 export class UserCancelled extends Error {}
 
@@ -37,30 +38,34 @@ export class FileService extends AbstractFileService {
   }
 
   saveFile(fileName: string, content: string) {
-    const saveFile = document.createElement("a");
-    saveFile.href = "data:application/octet-stream," + encodeURI(content);
-    saveFile.download = fileName;
-    saveFile.click();
+    const blob = new Blob([content], {type: "text/plain;charset=utf-8"});
+    FileSaver.saveAs(blob, fileName);
   }
 
-  getAnalyticDefinitions(fileType:string) : Promise<Array<{file: File, analyticDefinition: TransformerDefJsonInterface}>> {
-    // Create the load file dialog
+  loadFile(fileType: string, multiple?: boolean): Promise<FileList> {
     return new Promise<FileList>((resolve, reject) => {
-      let loadAnalyticFiles = document.createElement("INPUT") as HTMLInputElement;
-      loadAnalyticFiles.type = "file";
-      loadAnalyticFiles.accept = fileType;
-      loadAnalyticFiles.onerror = reject;
-      loadAnalyticFiles.onabort = reject;
-      loadAnalyticFiles.multiple = true;
-      loadAnalyticFiles.onchange = () => {
-        if (loadAnalyticFiles.files) {
-          resolve(loadAnalyticFiles.files);
+      const input = document.createElement("INPUT") as HTMLInputElement;
+      document.body.appendChild(input);
+      input.type = "file";
+      input.accept = fileType;
+      input.onerror = reject;
+      input.onabort = reject;
+      input.multiple = !!multiple;
+      input.onchange = () => {
+        if (input.files) {
+          resolve(input.files);
         } else {
           reject(new UserCancelled());
         }
       };
-      loadAnalyticFiles.click();
+      input.click();
+      document.body.removeChild(input);
     })
+  }
+
+  getAnalyticDefinitions(fileType:string) : Promise<Array<{file: File, analyticDefinition: TransformerDefJsonInterface}>> {
+    // Create the load file dialog
+    return this.loadFile(fileType, true)
       // Read the file(s)
       .map((analyticFile: File) => {
         return new Promise<{ file: File, content: string }>((resolve, reject) => {
@@ -103,42 +108,29 @@ export class FileService extends AbstractFileService {
           throw new Error(`Invalid @AnalyticDefinition in file: ${fileAnalytic.file.name}`);
         }
         return fileAnalytic;
-      })
+      });
   }
 
   getFileData(fileType:string) : Promise<{file: File, fileContent: string}> {
-    return new Promise<File>((resolve, reject) => {
-      let loadFile = document.createElement("INPUT") as HTMLInputElement;
-      loadFile.type = "file";
-      loadFile.accept = fileType;
-      loadFile.onerror = reject;
-      loadFile.onabort = reject;
-      loadFile.onchange = () => {
-        if (loadFile.files && loadFile.files[0]) {
-          resolve(loadFile.files[0]);
-        } else {
-          reject(new UserCancelled());
-        }
-      };
-      loadFile.click();
-    })
-    .then(file => {
-      return new Promise<{file: File, fileContent: string}>((resolve, reject) => {
-        const fileReader = new FileReader();
-        fileReader.onload = function () {
-          resolve({
-            file: file,
-            fileContent: fileReader.result
-          });
-        };
-        fileReader.onerror = reject;
-        fileReader.onabort = reject;
-        fileReader.readAsText(file);
-        setTimeout(() => {
-          reject(new Error("Timed out while reading file"));
-          fileReader.abort();
-        }, 2000);
+    return this.loadFile(fileType)
+      .then(files => files[0])
+      .then(file => {
+        return new Promise<{file: File, fileContent: string}>((resolve, reject) => {
+          const fileReader = new FileReader();
+          fileReader.onload = function () {
+            resolve({
+              file: file,
+              fileContent: fileReader.result
+            });
+          };
+          fileReader.onerror = reject;
+          fileReader.onabort = reject;
+          fileReader.readAsText(file);
+          setTimeout(() => {
+            reject(new Error("Timed out while reading file"));
+            fileReader.abort();
+          }, 2000);
+        });
       });
-    });
   }
 }
